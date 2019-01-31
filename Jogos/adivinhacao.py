0d51895a6758415b45496e309a3fbd43d821803a
root <root@localhost.localdomain>
2019-01-30 23:50:07 -0200

    adicionado pontuação

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -1,10 +1,24 @@
+import random
+
 print("************************************")
 print("* Bem vindo ao jogo da adivinhação *")
 print("************************************")
 
-numero_secreto = 42
-numero_de_tentativas = 3
+numero_secreto = random.randrange(1,101)
+numero_de_tentativas = 0
 rodada = 1
+pontos = 1000
+
+print("Digite o nivel de difiuldade:")
+print("1 - Facil | 2 - medio | 3 dificil")
+dificuldade = int(input())
+
+if(dificuldade == 1):
+    numero_de_tentativas = 20
+elif(dificuldade == 2):
+    numero_de_tentativas = 10
+else:
+    numero_de_tentativas = 5
 
 while(rodada <= numero_de_tentativas):
     chute_str = input("Digite o seu numero: ")
@@ -19,13 +33,17 @@ while(rodada <= numero_de_tentativas):
     menor = chute < numero_secreto
 
     if (certo):
-        print("Você acertou")
+        print("Você acertou e fez {} pontos".format(pontos))
+        
         break
     else:
         if(maior):
             print("Você errou, chutou um valor maior que o número secreto")
         elif(menor):
             print("Você errou, chutou um valor menor que o número secreto")
+
+    pontos_rodada = abs(chute - numero_secreto)
+    pontos = pontos - pontos_rodada
 
     rodada = rodada + 1
 
