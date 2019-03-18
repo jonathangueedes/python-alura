2ee5d757ba9b770448a3b69ec4de7a7def5ca884
Jonathan <jonathangueedes@gmail.com>
2019-03-17 22:48:57 -0300

    adicionado novas funcionalidades

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,15 +1,77 @@
+import random
 def jogar_forca():
 
-    print("******************************")
-    print("* Bem vindo ao jogo da Forca *")
-    print("******************************")
-    print("Fim do jogo")
+    print("********************************")
+    print("** Bem vindo ao jogo da Forca **")
+    print("********************************")
 
-    numero_secreto = 9
+    print("A sua palavra está logo abaixo, Boa sorte!!!")
+    print()
 
-    print(numero_secreto)
+    arquivo = open("/home/jonathan/python-alura/Jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    
+    letras_acertadas = []
+    for letra in palavra_secreta:
+        letras_acertadas.append("_")
+    print(letras_acertadas)
+
+    print()
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+        
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        
+        else:
+            erros = erros + 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+
+
+    if(acertou):
+        print("Voce acertou")
+    else:
+        print("voce errou")
+
+    print()    
+    print("______________")
+    print("Fim de Jogo")
+
+
+        
+
+
+
+
+
+
+
 
 if(__name__ == "__main__"):
     jogar_forca()
-
 
